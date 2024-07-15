621182c822cd19bc6cb3180a4b7f7474db12a5dd
rami242424 <mickygamja@naver.com>
2024-07-15 11:04:08 +0900

    list 불러오기 fetch 및 a태그 Link to로 바꾸기

--- workspace/js/src/pages/community/Detail.jsx
+++ workspace/js/src/pages/community/Detail.jsx
@@ -1,22 +1,47 @@
 import Submit from "@/components/Submit";
 import CommentList from "@/pages/community/CommentList";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+
+// 게시물 목록 찾아서 응답하게 만드는 것
+async function fetchPost(_id){
+  const url = `https://api.fesp.shop/posts/${_id}`;
+  const res = await fetch(url); // 비동기함수 : 호출하자마자 리턴(작업 완료되기 전에)
+  return res.json();
+}
 
 export default function Detail(){
+
+  const { _id, type } = useParams(); // type을 꺼내기 위해서 사용
+  
+  const [data, setData] = useState(null);
+
+  const fetchData = async (_id) => {
+    const result = await fetchPost(_id);
+    setData(result.item);
+  }
+
+  
+  useEffect(() => { // 화면에 렌더링 되고 난 후 효과들(함수가 최종 리턴을 한후에 useEffect 안에서 동작)
+    fetchData(_id);
+  }, []);
+
+
   return (
     <main className="container mx-auto mt-4 px-4">
       <section className="mb-8 p-4">
-        <form action="/info">
-        <div className="font-semibold text-xl">제목 : 좋은 소식이 있습니다.</div>
-          <div className="text-right text-gray-400">작성자 : 제이지</div>
+        <form action={`/${type}`}>
+        <div className="font-semibold text-xl">제목 : { data?.title }</div>
+          <div className="text-right text-gray-400">작성자 : { data?.user.name }</div>
           <div className="mb-4">
             <div>
-              <pre className="font-roboto w-full p-2 whitespace-pre-wrap">좋은 소식을 가지고 왔습니다.<br />오늘 드디어 최종 면접을 합니다.<br />많이 응원해 주세요^^</pre>
+              <pre className="font-roboto w-full p-2 whitespace-pre-wrap">{ data?.content }</pre>
             </div>
             <hr/>
           </div>
           <div className="flex justify-end my-4">
-            <a href="/info" className="bg-orange-500 py-1 px-4 text-base text-white font-semibold ml-2 hover:bg-amber-400 rounded">목록</a>
-            <a href="/info/1/edit" className="bg-gray-900 py-1 px-4 text-base text-white font-semibold ml-2 hover:bg-amber-400 rounded">수정</a>
+            <Link to={`/${type}`} className="bg-orange-500 py-1 px-4 text-base text-white font-semibold ml-2 hover:bg-amber-400 rounded">목록</Link>
+            <Link to={`/${type}/${_id}/edit`} className="bg-gray-900 py-1 px-4 text-base text-white font-semibold ml-2 hover:bg-amber-400 rounded">수정</Link>
             <Submit bgColor="red">삭제</Submit>
           </div>
         </form>
